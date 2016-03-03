0cce056812b0c98be88942e09c68a733d6e12117
koral <koral>
2016-03-03 02:35:26 +0100

    native_peer

--- app/src/main/java/pl/droidsonroids/ndkdemo/Demo.java
+++ app/src/main/java/pl/droidsonroids/ndkdemo/Demo.java
@@ -4,5 +4,21 @@ public class Demo {
 	static {
 		System.loadLibrary("demo");
 	}
+
+	private long mNativeDemo;
+
 	public native String getHelloWorld();
+
+	private native long init();
+
+	private native void release(long ptr);
+
+	public Demo() {
+		mNativeDemo = init();
+	}
+
+	public void release() {
+		release(mNativeDemo);
+		mNativeDemo = 0;
+	}
 }
--- app/src/main/jni/demo.c
+++ app/src/main/jni/demo.c
@@ -1,6 +1,21 @@
 #include<jni.h>
+#include<stdlib.h>
+
+typedef struct {
+
+} Demo;
 
 JNIEXPORT jstring JNICALL
-Java_pl_droidsonroids_ndkdemo_Demo_getHelloWorld(JNIEnv *env, jobject object) {
+Java_pl_droidsonroids_ndkdemo_Demo_getHelloWorld(JNIEnv *env, jobject instance) {
 	return (*env)->NewStringUTF(env, "hello world");
+}
+
+JNIEXPORT jlong JNICALL
+Java_pl_droidsonroids_ndkdemo_Demo_init(JNIEnv *env, jobject instance) {
+	return (jlong) malloc(sizeof(Demo));
+}
+
+JNIEXPORT void JNICALL
+Java_pl_droidsonroids_ndkdemo_Demo_release__J(JNIEnv *env, jobject instance, jlong ptr) {
+	free((void *) ptr);
 }
