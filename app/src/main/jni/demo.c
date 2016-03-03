f0516273ad99b1b25fcc73764d813fa56652c99d
koral <koral>
2016-03-03 02:49:25 +0100

    simple_log

--- app/src/main/jni/demo.c
+++ app/src/main/jni/demo.c
@@ -1,5 +1,8 @@
 #include<jni.h>
 #include<stdlib.h>
+#include <android/log.h>
+
+#define  LOG_TAG    "demo"
 
 typedef struct {
 
@@ -7,15 +10,16 @@ typedef struct {
 
 JNIEXPORT jstring JNICALL
 Java_pl_droidsonroids_ndkdemo_Demo_getHelloWorld(JNIEnv *env, jobject instance) {
-	return (*env)->NewStringUTF(env, "hello world");
+    __android_log_print(ANDROID_LOG_FATAL, LOG_TAG, "hello %s %p", "world", &instance);
+    return (*env)->NewStringUTF(env, "hello world");
 }
 
 JNIEXPORT jlong JNICALL
 Java_pl_droidsonroids_ndkdemo_Demo_init(JNIEnv *env, jobject instance) {
-	return (jlong) malloc(sizeof(Demo));
+    return (jlong) malloc(sizeof(Demo));
 }
 
 JNIEXPORT void JNICALL
 Java_pl_droidsonroids_ndkdemo_Demo_release__J(JNIEnv *env, jobject instance, jlong ptr) {
-	free((void *) ptr);
+    free((void *) ptr);
 }
