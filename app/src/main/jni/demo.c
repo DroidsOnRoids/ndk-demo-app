58057acdcfde98952d99deddf7b6203e52ba9fe5
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2016-03-31 17:45:23 +0200

    pthread fixed

--- app/src/main/jni/demo.c
+++ app/src/main/jni/demo.c
@@ -10,27 +10,40 @@ typedef struct {
 
 } Demo;
 
+JavaVM *vm;
+jobject globalInstance;
+
 static void *doWork(void *unused) {
-    return NULL;
+	JNIEnv *env;
+	struct JavaVMAttachArgs args = {
+			.group=NULL,
+			.name="NativeWorker",
+			.version = JNI_VERSION_1_6
+	};
+	(*vm)->AttachCurrentThread(vm, &env, &args);
+	jclass clazz = (*env)->GetObjectClass(env, globalInstance);
+	jmethodID methodID = (*env)->GetMethodID(env, clazz, "getHash", "()I");
+	jint hash = (*env)->CallIntMethod(env, globalInstance, methodID);
+	LOGE("hash %d", hash);
+	(*vm)->DetachCurrentThread(vm);
+	return NULL;
 }
 
 JNIEXPORT jstring JNICALL
 Java_pl_droidsonroids_ndkdemo_Demo_getHelloWorld(JNIEnv *env, jobject instance) {
-    pthread_t thread;
-    pthread_create(&thread, NULL, doWork, NULL);
-    jclass clazz = (*env)->GetObjectClass(env, instance);
-    jmethodID methodID = (*env)->GetMethodID(env, clazz, "getHash", "()I");
-    jint hash = (*env)->CallIntMethod(env, instance, methodID);
-    LOGE("hash %d", hash);
-    return (*env)->NewStringUTF(env, "hello world");
+	globalInstance = (*env)->NewGlobalRef(env, instance);
+	(*env)->GetJavaVM(env, &vm);
+	pthread_t thread;
+	pthread_create(&thread, NULL, doWork, NULL);
+	return (*env)->NewStringUTF(env, "hello world");
 }
 
 JNIEXPORT jlong JNICALL
 Java_pl_droidsonroids_ndkdemo_Demo_init(JNIEnv *env, jobject instance) {
-    return (jlong) malloc(sizeof(Demo));
+	return (jlong) malloc(sizeof(Demo));
 }
 
 JNIEXPORT void JNICALL
 Java_pl_droidsonroids_ndkdemo_Demo_release__J(JNIEnv *env, jobject instance, jlong ptr) {
-    free((void *) ptr);
+	free((void *) ptr);
 }
